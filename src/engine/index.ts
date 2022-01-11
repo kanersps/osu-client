b51e23d45ac30f024f9697b55b305134367753fa
Kane Petra <kane.petra@looplang.org>
2022-01-11 20:06:11 +0100

    tweak: ignore hotel assets

--- src/engine/index.ts
+++ src/engine/index.ts
@@ -1,4 +1,6 @@
 import { Application as PixiApplication } from "pixi.js";
+import AssetManager from "./assets/AssetManager";
+import Room from "./room";
 
 class RenderEngine {
   _app: PixiApplication;
@@ -10,6 +12,17 @@ class RenderEngine {
     });
 
     this._app.start();
+
+    // Create test room
+    const room = new Room(`
+0000
+0##0
+0000
+`);
+
+    // Render room floor
+    AssetManager.loadFloor("assets/floor.png");
+    AssetManager.loadAllResources();
   }
 
   get view() {
